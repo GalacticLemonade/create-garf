9039e27473aab9a586d4094e46ca6eeaf5668aa8
GalacticLemonade <aidenmoore1820@gmail.com>
2024-03-09 11:29:47 -0700

    Add rest of recipes

--- src/main/java/dev/galacticlemon/creategarf/CreateGarfRecipes.java
+++ src/main/java/dev/galacticlemon/creategarf/CreateGarfRecipes.java
@@ -1,6 +1,7 @@
 package dev.galacticlemon.creategarf;
 
 import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
@@ -26,7 +27,41 @@ public class CreateGarfRecipes extends FabricRecipeProvider {
                 .requires(Blocks.REDSTONE_WIRE)
                 .requires(Blocks.OBSIDIAN)
                 .group("create")
-                .unlockedBy("obsidian", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(Blocks.OBSIDIAN).build()))
+                .unlockedBy("andesite_casing", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(AllBlocks.ANDESITE_CASING).build()))
+                .save(exporter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CreateGarfBlocks.FREEWHEEL_CLUTCH)
+                .requires(AllBlocks.ANDESITE_CASING)
+                .requires(AllBlocks.SHAFT)
+                .requires(AllItems.IRON_SHEET)
+                .requires(AllBlocks.COGWHEEL)
+                .group("create")
+                .unlockedBy("andesite_casing", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(AllBlocks.ANDESITE_CASING).build()))
+                .save(exporter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CreateGarfBlocks.OVERSTRESS_CLUTCH)
+                .requires(AllBlocks.ANDESITE_CASING)
+                .requires(AllBlocks.SHAFT)
+                .requires(AllItems.IRON_SHEET)
+                .requires(AllItems.ELECTRON_TUBE)
+                .group("create")
+                .unlockedBy("electron_tube", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(AllItems.ELECTRON_TUBE).build()))
+                .save(exporter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CreateGarfBlocks.CENTRIFUGAL_CLUTCH)
+                .requires(AllBlocks.ANDESITE_CASING)
+                .requires(AllBlocks.SHAFT)
+                .requires(AllItems.IRON_SHEET)
+                .requires(AllBlocks.SPEEDOMETER)
+                .group("create")
+                .unlockedBy("speedometer", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(AllBlocks.SPEEDOMETER).build()))
+                .save(exporter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, CreateGarfBlocks.INVERTED_CENTRIFUGAL_CLUTCH)
+                .requires(Blocks.REDSTONE_TORCH)
+                .requires(CreateGarfBlocks.CENTRIFUGAL_CLUTCH)
+                .group("create")
+                .unlockedBy("centrifugal_clutch", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(CreateGarfBlocks.CENTRIFUGAL_CLUTCH).build()))
                 .save(exporter);
     }
 }
